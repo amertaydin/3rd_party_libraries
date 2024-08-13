7dc7ad9d88bd64e084a0ddce9b24643ad9a7a916
amertaydin <amertaydin14@gmail.com>
2024-08-13 13:30:56 +0200

    usage of 3rd party libraries

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,33 @@
-// Code goes here!
+import { Product } from "./product.model";
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
+import { validate } from "class-validator";
+
+const products = [
+  { title: "A Carper", price: 29.99 },
+  { title: "A Book", price: 10.99 },
+];
+
+// const p1 = new Product("A Book", 12.99);
+
+// const loadedProducts = products.map((prod) => {
+//   return new Product(prod.title, prod.price);
+// });
+
+const newProd = new Product("", -5.99);
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.log("Validation Errors!");
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
+
+const loadedProducts = plainToClass(Product, products);
+
+// for (const prod of loadedProducts) {
+//   console.log(prod.getInformation());
+// }
+
+console.log(loadedProducts);
